656180e3f66d7f97748374b2902321f05ef061ed
Luzaks <kanemekanik@gmail.com>
2020-05-26 15:34:36 -0500

    Feat: Use hooks and add API service for displaying on screen images components.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  const GIFS = [
-    'https://media2.giphy.com/media/xT8qBepJQzUjXpeWU8/giphy.gif?cid=ecf05e47ffdc46371e75039a4517f00a2127ca3da0636e96&rid=giphy.gif',
-    'https://media1.giphy.com/media/n5VaQoW39Z9S0/200w.webp?cid=ecf05e47f4e76fe7ab5e5fc7f37461cad557094e9cbdf68c&rid=200w.webp'
-  ];
-
+  const urlAPI =  'https://api.giphy.com/v1/gifs/search?api_key=Cjgi2R0t4kg36Y9hoxpkqYiEqg22KdTd&q=panda&limit=10&offset=0&rating=G&lang=en';
+  const [gifs, setGifs] = useState([]);
+  useEffect(function(){
+      fetch(urlAPI,  { mode: 'cors' })
+          .then(response => response.json())
+          .then((response) => {
+              const {data} = response;
+              const gifs = data.map(
+                  image => image.images.downsized_medium.url
+              );
+              setGifs(gifs);
+          })
+  }, []);
   return (
     <div className="App">
       <section className="App-header">
-
+          {
+              gifs.map(singleGif =>
+                  <img src={singleGif} alt="gift"/>
+              )
+          }
       </section>
     </div>
   );
